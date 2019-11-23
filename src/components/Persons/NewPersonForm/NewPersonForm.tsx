ee1791e08c36e6c2f8f79f1bb217fed10f3d54f8
Ronen Agranat <ronen.agranat@gmail.com>
2019-11-23 20:08:50 +0100

    Add submit create person form logic
    * Add 'role' field
    
    TODO
    
    * Modify server to role no minutes for new person

--- src/components/Persons/NewPersonForm/NewPersonForm.tsx
+++ src/components/Persons/NewPersonForm/NewPersonForm.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 import styles from './NewPersonForm.module.css';
+import FaceToFace from '../../../services/FaceToFace';
+import { AxiosError, AxiosResponse } from 'axios';
 
 interface INewPersonFormValues {
   name?: string | undefined;
+  role?: string | undefined;
 }
 
 interface INewPersonFormErrors {
   name?: string | undefined;
+  role?: string | undefined;
 }
 
 const NewPersonForm = () => {
@@ -20,25 +24,45 @@ const NewPersonForm = () => {
         if (!values.name) {
           errors.name = 'Name is required';
         }
+        if (!values.role) {
+          errors.role = 'Role is required';
+        }
 
         return errors;
       }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
+      onSubmit={(values: INewPersonFormValues, { setSubmitting }) => {
+        FaceToFace.post('persons', {
+          name: values.name,
+          role: values.role,
+          imageUrl: 'https://picsum.photos/200/300',
+        })
+          .then((response: AxiosResponse) => {
+            console.log('Got create person response', response);
+            setSubmitting(false);
+          }).catch((error: AxiosError) => {
+          console.error('Something went wrong creating person', error);
           setSubmitting(false);
-        }, 400);
+        });
       }}
     >
       {({ isSubmitting }) => (
         <Form>
           <h1>New person form</h1>
-          <label>Name</label>
-          <Field type="name" name="name"/>
-          <ErrorMessage name="name" component="div"/>
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
+          <div>
+            <label>Name</label>
+            <Field name="name"/>
+            <ErrorMessage name="name" component="div"/>
+          </div>
+          <div>
+            <label>Role</label>
+            <Field name="role"/>
+            <ErrorMessage name="role" component="div"/>
+          </div>
+          <div>
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </div>
         </Form>
       )}
     </Formik>
