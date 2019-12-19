71505c28cc10b04f02004867e39e83403f04f9d9
Ronen Agranat <ronen.agranat@gmail.com>
2019-12-19 19:18:09 +0100

    Remove log statement and prettify

--- src/components/Minutes/MinutesEditor/MinutesTextArea/MinutesTextArea.tsx
+++ src/components/Minutes/MinutesEditor/MinutesTextArea/MinutesTextArea.tsx
@@ -21,72 +21,74 @@ const BULLET_CHARACTERS = ['-', '•', '*'];
 // "Output filter"
 const parseNotesText = (text: string): string => {
   let newText = text;
-  newText = newText.replace(/^(\s*)[-*]/mg, `$1${BULLET_CHARACTERS[0]}`);
+  newText = newText.replace(/^(\s*)[-*]/gm, `$1${BULLET_CHARACTERS[0]}`);
   return newText;
 };
 
-const MinutesTextArea = forwardRef((props: IOneOnOneProps, ref: Ref<HTMLTextAreaElement>) => {
-  const [notesValue, setNotesValue] = useState(props.notes);
+const MinutesTextArea = forwardRef(
+  (props: IOneOnOneProps, ref: Ref<HTMLTextAreaElement>) => {
+    const [notesValue, setNotesValue] = useState(props.notes);
 
-  const noteArea = props.active ? (
-    <textarea
-      ref={ref}
-      className={styles.TextAreaEdit}
-      onChange={(event: ChangeEvent<HTMLTextAreaElement>) => {
-        // Indent level
-        let text = event.target.value;
+    const noteArea = props.active ? (
+      <textarea
+        ref={ref}
+        className={styles.TextAreaEdit}
+        onChange={(event: ChangeEvent<HTMLTextAreaElement>) => {
+          // Indent level
+          let text = event.target.value;
 
-        const selectionStart = event.target.selectionStart;
-        let indentLevel = 0;
-        let addedIndent = false;
-        // Only maintain indent if string is getting longer (enter not backspace)
-        if (text.length > notesValue.length) {
-          // Only the part of the text up until the cursor should be processed
-          let prefix = text.slice(0, selectionStart);
-          let suffix = text.slice(selectionStart);
-          [text, indentLevel, addedIndent] = parseInputText(prefix);
-          text = text.concat(suffix);
-        }
+          const selectionStart = event.target.selectionStart;
+          let indentLevel = 0;
+          let addedIndent = false;
+          // Only maintain indent if string is getting longer (enter not backspace)
+          if (text.length > notesValue.length) {
+            // Only the part of the text up until the cursor should be processed
+            let prefix = text.slice(0, selectionStart);
+            let suffix = text.slice(selectionStart);
+            [text, indentLevel, addedIndent] = parseInputText(prefix);
+            text = text.concat(suffix);
+          }
 
-        props.notesChanged(text);
-        setNotesValue(text);
+          props.notesChanged(text);
+          setNotesValue(text);
 
-        if (addedIndent) {
-          // FIXME: There is an edge case here that is not functioning correctly.
-          // It is not getting invoked when on the last two lines:
-          // - select last character position
-          // click bullet on previous line
-          // press enter
-          // indent is created
-          // but position is not restored
+          if (addedIndent) {
+            // FIXME: There is an edge case here that is not functioning correctly.
+            // It is not getting invoked when on the last two lines:
+            // - select last character position
+            // click bullet on previous line
+            // press enter
+            // indent is created
+            // but position is not restored
 
-          // Restore the selection of the caret
-          // props.restoreSelection(selectionStart + indentLevel);
-          if (ref && typeof ref === 'object') {
-            if (ref.current) {
-              console.log('setting position and value');
-              ref.current.value = text;
-              ref.current.selectionStart = ref.current.selectionEnd = selectionStart + indentLevel;
+            // Restore the selection of the caret
+            // props.restoreSelection(selectionStart + indentLevel);
+            if (ref && typeof ref === 'object') {
+              if (ref.current) {
+                ref.current.value = text;
+                ref.current.selectionStart = ref.current.selectionEnd =
+                  selectionStart + indentLevel;
+              }
             }
           }
-        }
-      }}
-      value={notesValue}
-      placeholder={props.title}
-    />
-  ) : (
-    <div className={styles.TextAreaShow} onClick={props.focused}>
-      {Boolean(notesValue.length) ? parseNotesText(notesValue) : props.title}
-    </div>
-  );
+        }}
+        value={notesValue}
+        placeholder={props.title}
+      />
+    ) : (
+      <div className={styles.TextAreaShow} onClick={props.focused}>
+        {Boolean(notesValue.length) ? parseNotesText(notesValue) : props.title}
+      </div>
+    );
 
-  return (
-    <div>
-      <h4>{props.title}</h4>
-      <div className={styles.MinutesTextArea}>{noteArea}</div>
-    </div>
-  );
-});
+    return (
+      <div>
+        <h4>{props.title}</h4>
+        <div className={styles.MinutesTextArea}>{noteArea}</div>
+      </div>
+    );
+  },
+);
 
 MinutesTextArea.propTypes = {
   notesChanged: PropTypes.func.isRequired,
