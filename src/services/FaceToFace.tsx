9533932108ef10293c3bbe53594a2381caead8eb
Ronen Agranat <ronen.agranat@gmail.com>
2021-03-22 22:00:18 +0100

    WIP: Add axios-jwt

--- src/services/FaceToFace.tsx
+++ src/services/FaceToFace.tsx
@@ -1,7 +1,34 @@
 import axios from 'axios';
+import { IAuthTokens, TokenRefreshRequest, useAuthTokenInterceptor } from 'axios-jwt'
 
-const FaceToFace = axios.create({
-  baseURL: process.env.REACT_APP_F2F_SERVER_ENDPOINT || 'http://localhost:3001'
+// https://github.com/jetbridge/axios-jwt
+
+const baseURL = process.env.REACT_APP_F2F_SERVER_ENDPOINT || 'http://localhost:3001';
+
+export const FaceToFace = axios.create({
+  baseURL
 });
 
+export interface IAuthResponse {
+  accessToken: string;
+  refreshToken: string;
+}
+
+const refreshEndpoint = `${baseURL}/auth/refresh`;
+
+export const authResponseToAuthTokens = (res: IAuthResponse): IAuthTokens => ({
+  accessToken: res.accessToken,
+  refreshToken: res.refreshToken
+})
+
+// define token refresh function
+const requestRefresh: TokenRefreshRequest = async (refreshToken: string): Promise<string> => {
+  // perform refresh
+  return (await axios.post(refreshEndpoint, { refreshToken })).data.accessToken
+}
+
+// add interceptor to your axios instance
+useAuthTokenInterceptor(FaceToFace, { requestRefresh })
+
+// TODO: Prefer named export
 export default FaceToFace;
