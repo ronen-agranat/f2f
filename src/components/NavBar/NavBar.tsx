33e974355dbaa27d9138b6f699470d2e29f2977e
Ronen Agranat <ronen.agranat@gmail.com>
2021-03-23 20:28:16 +0100

    Add logout link

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import styles from './NavBar.module.css';
 import { Link } from 'react-router-dom';
+import { clearAuthTokens } from 'axios-jwt';
 
 const NavBar = () => {
+  const logout = useCallback(() => {
+    clearAuthTokens();
+  }, [])
+
   return (
     <div className={styles.NavBar}>
       <nav>
         <ul>
           <li>
             <Link to="/persons">People</Link>
+          </li>
+          <li>
+            <Link to="/" onClick={logout}>Log Out</Link>
           </li>
         </ul>
       </nav>
